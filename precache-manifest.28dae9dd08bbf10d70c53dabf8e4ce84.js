6fd5b98c3aea1e23714a0bd6088335293d950c1b
zionjke <vincere802@gmail.com>
2020-07-01 21:07:23 +0300

    Updates

---
+++ precache-manifest.28dae9dd08bbf10d70c53dabf8e4ce84.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5e761d122310bd63276e9ea5855ac5",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "6d007c24d53da36a8bf8",
+    "url": "/toDoList/static/css/main.7d3f48f9.chunk.css"
+  },
+  {
+    "revision": "f35782a317cb8f238972",
+    "url": "/toDoList/static/js/2.045ce598.chunk.js"
+  },
+  {
+    "revision": "6d007c24d53da36a8bf8",
+    "url": "/toDoList/static/js/main.e36efe38.chunk.js"
+  },
+  {
+    "revision": "9722a55e69eb4ad6e976",
+    "url": "/toDoList/static/js/runtime~main.930cf86c.js"
+  },
+  {
+    "revision": "78cc821587c8145935bfc5b01738d502",
+    "url": "/toDoList/static/media/add.78cc8215.svg"
+  },
+  {
+    "revision": "1175686f65f8ace80847eb94547123c0",
+    "url": "/toDoList/static/media/close.1175686f.svg"
+  },
+  {
+    "revision": "75c0363f4549f3f1fad56aa634acf055",
+    "url": "/toDoList/static/media/log-out.75c0363f.png"
+  },
+  {
+    "revision": "a181d29b1774868d6e955f4b92689145",
+    "url": "/toDoList/static/media/login.a181d29b.png"
+  },
+  {
+    "revision": "b36a3633ba2e4be2ec54e7b20ba43f53",
+    "url": "/toDoList/static/media/remove.b36a3633.svg"
+  },
+  {
+    "revision": "c2427492b04583164af0f709e2033860",
+    "url": "/toDoList/static/media/star.c2427492.svg"
+  }
+]);
