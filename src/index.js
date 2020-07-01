19a20e0eed6ac809ceaf3dfab0535b1497ed86c9
zionjke <vincere802@gmail.com>
2020-07-01 21:25:16 +0300

    add gh pages +

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.scss';
 import App from './App';
 import store from "./redux/store";
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root'));
 
 
