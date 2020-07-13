5fd5a9097f3c81064471ad85871098bb20d516b4
zionjke <vincere802@gmail.com>
2020-07-13 20:47:11 +0300

    пофиксил className

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, {createRef, useRef} from 'react'
+import React, {createRef} from 'react'
 import {connect} from "react-redux";
 import {login} from "../../redux/loginReducer";
 import {statuses} from "../../redux/loginReducer";
